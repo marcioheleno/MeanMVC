157d18fcffde9aeae3e59690e02668595263101d
marcioheleno <marcio@marcioheleno.website>
2016-05-12 22:03:11 -0300

    commit inicial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var home = require('../controllers/home');
+var inforlocal = require('../controllers/inforlocal');
+var comentario = require('../controllers/comentarios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var about = require('../controllers/about');
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// routers
+
+// page => home
+router.get('/', home.homelist);
+// page => inforlocal
+router.get('/inforlocal', inforlocal.locationInfo);
+// page => inforlocal => comentario
+router.get('/inforlocal/comentario', comentario.comentario);
+
+// page => about
+router.get('/about', about.other);
 
 module.exports = router;
